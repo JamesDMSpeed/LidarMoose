9487cf5426f4afba3e3236d062ae77436c361f93
ingridsnoan <ingribsn@ntnu.no>
2018-08-31 12:05:51 +0200

    I've been going through the script. I've made some notes for my own understanding, and I have also written down some problems I need to figure out. I understood that I was going to make an "issue" if I had trouble, but don't know how to do that yet.

--- lidarRtests.R
+++ lidarRtests.R
@@ -1,12 +1,13 @@
 #LidR
+install.packages("lidR")
+installed.packages("raster")
+install.packages("rasterVis")
 
 require(lidR)
 require(raster)
 require(rasterVis)
 
 
-
-#testtest
 #Import data
 #Clips of area around Bratsberg exclosure sites. Converted from laz to las using laszip.exe
 #Trondheim 2010 data
@@ -17,7 +18,7 @@ require(rasterVis)
 
 #Exclosure site - 2014 data
 #lidtest2<-readLAS('S:\\Anders Kolstad\\lidar\\Trondheim2014\\data\\32-1-511-213-45.las')
-# lidtest2<-readLAS('LidarTest\\32-1-511-213-45.las')
+#lidtest2<-readLAS('LidarTest\\32-1-511-213-45.las')
 # plot(lidtest2)
 # plot(lidtest2,trim=0.9)#Omit highest 10% of records in colour scheme (birds)
 # 
@@ -35,26 +36,30 @@ lidclip
 plot(lidclip)
 
 #Clip to exclosure and open plots
-lidexclosure<-lasclipRectangle(lidtest2,573675,7025681,573695,7025701)
+#lidexclosure<-lasclipRectangle(lidtest2,573675,7025681,573695,7025701)
+lidexclosure<-lasclipRectangle(lidclip,573675,7025681,573695,7025701)
 plot(lidexclosure)
 #plot(lidexclosure,trim=0.9)
-lidopen<-lasclipRectangle(lidtest2,573751,7025718,573771,7025738)
+#lidopen<-lasclipRectangle(lidtest2,573751,7025718,573771,7025738)
+lidopen<-lasclipRectangle(lidclip,573751,7025718,573771,7025738)
 plot(lidopen)
 
-#Remove the tallest trees
+#Remove the tallest trees -why?
 lidex1<-lidexclosure
 hist(lidexclosure@data$Z)
+#Does the line above extract the z-coordinate data from lidexclosure? 
 lidex1@data[lidex1@data$Z>245]<-NA
+#Make all of the tree heights larger than 245 NA?
 plot(lidex1)
-
-#Make canopy and terrain TIN models for each treatment
+?grid_terrain
+#Make canopy and terrain TIN models for each treatment. TIN:representation if a continous surface.
 terrainmod<-grid_terrain(lidexclosure,method='knnidw',res=1)
 canopymod<-grid_canopy(lidexclosure,res=1)
 plot(canopymod-terrainmod)#Error
 #Merge together based on X and Y coordinates
 mergedf<-merge(terrainmod,canopymod,by=c('X','Y'))
 mergedf$Canopy<-mergedf$Z.y-mergedf$Z.x
-plot3d(mergedf$X,mergedf$Y,mergedf$Canopy)
+plot3d(mergedf$X,mergedf$Y,mergedf$Canopy) #error: object not supported
 
 gfex<-grid_metrics3d(lidexclosure,func=max(Z)-min(Z),res=1)
 gfopen<-grid_metrics3d(lidopen,func=max(Z)-min(Z),res=1)
@@ -81,11 +86,11 @@ rterropen<-as.raster(grid_terrain(lidopen,method='knnidw',res=0.5))
 rcanopen<-as.raster(grid_canopy(lidopen,res=0.5))
 plot(rcanopen-crop(rterropen,rcanopen))
 rtreesopen<-rcanopen-crop(rterropen,rcanopen)
-plot3d(rtreesopen)
-
+plot3d(rtreesopen)#looks a bit smaller then the exclosed plot, doesn't look like 20x20.
+?cellStats
 #Remove preexisting trees (>7m)
-#rtreesex[rtreesex>7]<-NA
-#rtreesopen[rtreesopen>7]<-NA
+rtreesex[rtreesex>7]<-NA
+rtreesopen[rtreesopen>7]<-NA
 plot(rtreesex)
 plot(rtreesopen)
 cellStats(rtreesex,median)
@@ -101,10 +106,17 @@ extent(rex)<-c(0,20,0,20)
 extent(rop)<-c(0,20,0,20)
 rex
 rop
+r2 <- extend(rex,rop)
+r2
+#r1 <- resample(rex,rop)
+#plot(r1)
+
+#can't make this last part work. Stack - make rex and rop a single vector. Error in compareRaster(x) : different number or columns
+#tried to resample. tried extend. But then there is no need to stack?
 stack1<-stack(rex,rop)
 names(stack1)<-c('Exclosure','Open')
-mytheme
+mytheme #whats mytheme?
 levelplot(stack1,par.settings='viridisTheme')
 
 bwplot(stack1,ylab='Canopy height (m) at 0.5m resoltion')
-
+?resample
