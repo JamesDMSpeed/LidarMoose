a3513a56257b07c4e532fe09e970cc9ac222f185
ingridsnoan <ingribsn@ntnu.no>
2018-09-18 09:26:26 +0200

    Functions to clip las files to polygons - using chull to convert unordered points into full polygons

--- Clipping_script.R
+++ Clipping_script.R
@@ -15,25 +15,24 @@ require(rasterVis)
 #Import plot coords
 plotcoords<-read.csv('Troendelag_20m_flater_pkt.csv',header=T,sep=';',dec=',')
 
-bratsbserg_b_poly<-Polygon(as.matrix(plotcoords[plotcoords$Name=='Brb',4:5]))
-bratsbserg_b_poly
-
 bratsberglas <-  readLAS('C:/Users/Ingrid/Documents/Master - Sustherb/orginale_las/bratsberg.las')
 bratsberglas 
 plot(bratsberglas)
 
-bbx<-lasclip(bratsberglas,bratsbserg_b_poly) #what does this one do?
-bbx
-plot(bbx)
+#This function tells the order of the points to make a complete ring
+bratsberg_b_order<-chull(as.matrix(plotcoords[plotcoords$Name=='Brb',4:5]))
+#This function makes a polygon using the bratsberg_b coordinates, ordered by the convex hull
+bratsberg_b_poly<-Polygon(as.matrix(plotcoords[plotcoords$Name=='Brb',4:5][bratsberg_b_order,]))
+#This one clips the las to the polygon
+bratsberg_b_plot<-lasclip(bratsberglas,bratsberg_b_poly)
+plot(bratsberg_b_plot)
+bratsberg_b_plot
 
+bratsberg_ub_order<-chull(as.matrix(plotcoords[plotcoords$Name=='Brub',4:5]))
+bratsberg_ub_poly<-Polygon(as.matrix(plotcoords[plotcoords$Name=='Brub',4:5][bratsberg_ub_order,]))
+bratsberg_ub_plot<-lasclip(bratsberglas,bratsberg_ub_poly)
+plot(bratsberg_ub_plot)
+bratsberg_ub_plot
 
-bratsberg_b <- lasclipPolygon(bratsberglas, bratsbserg_b_poly)
-#bratsberg_b<-lasclipRectangle(bratsberglas,573723,7025715,573750,7025741) #this one is not 20x20 m, it has an area of 700m^2. Bruk lasclipPoly
-#bratsberg_ub<-lasclipRectangle(bratsberglas,573675,7025681,573695,7025701)
-bratsberg_b
-plot(bratsberg_b)
-bratsberg_ub
-plot(bratsberg_ub)
-
-writeLAS(bratsberg_b,'clipped_las/bratsberg_b.las')
-writeLAS(bratsberg_ub,'clipped_las/bratsberg_ub.las')
+writeLAS(bratsberg_b_plot,'clipped_las/bratsberg_b.las')
+writeLAS(bratsberg_ub_plot,'clipped_las/bratsberg_ub.las')
