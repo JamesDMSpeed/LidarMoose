9a55987950b7fd34336c66d528ea4bc38d90d234
JamesDMSpeed <james.speed@ntnu.no>
2018-09-20 13:45:39 +0200

    Checking the tree segmentation in Bratsberg unbrowsed

--- Testing tree identification and masking.R
+++ Testing tree identification and masking.R
@@ -1,3 +1,6 @@
+require(lidR)
+require(raster)
+
 #Detecting trees
 #Testing with Bratsberg browsed
 
@@ -41,7 +44,7 @@ bclip<-lasclip(bb,treeout@polygons[[bigtrees[1]]]@Polygons[[1]],inside=F)
 for(i in 2:length(bigtrees)){
 print(i)
 bclip<-lasclip(bclip,treeout@polygons[[bigtrees[i]]]@Polygons[[1]],inside=F)}
-plot(bclip)#Didnt quite work... 
+plot(bclip) 
 
 plot(as.raster(grid_canopy(bclip,res=0.5))-(crop(as.raster(grid_terrain(bclip,method='knnidw',res=0.5)),as.raster(grid_canopy(bclip,res=0.5)))))
 
@@ -57,11 +60,27 @@ gridterrainbu<-as.raster(grid_terrain(bu,method='knnidw',res=0.5))
 #Difference between canopy and terrain = canopy height from ground
 canopydiffbu<-gridcanopybu-(crop(gridterrainbu,gridcanopybu))
 plot(canopydiffbu)
-treesub<-tree_detection(bu,ws=2,hmin=7)
-lastrees_dalponte(bu,canopydiffbu,treesub)#Dalponte algorthim... Using the canopy height difference (not canopy model)
+treesub<-tree_detection(bu,ws=5,hmin=5)
+treeheight_ub<-extract(canopydiffbu,treesub[,1:2])
+
+lastrees_dalponte(bu,canopydiffbu,treesub[treeheight_ub>=7,],th_seed=0.05,th_cr=0.1)#Dalponte algorthim... Using the canopy height difference (not canopy model)
 
 #Now make hulls around the trees
 treeoutub<-tree_hulls(bu,type='concave',field='treeID')
 
 plot(canopydiffbu)
 plot(treeoutub,add=T) # Not all of these are preexisting trees. Only some should be clipped..
+
+
+#Those greater than 7m
+bigtrees_ub<-which(extract(canopydiffbu,treeoutub,fun=max,na.rm=T)>4)
+
+#Clip out trees
+bclip_ub<-lasclip(bu,treeoutub@polygons[[bigtrees_ub[1]]]@Polygons[[1]],inside=F)
+  for(i in 2:length(bigtrees_ub)){
+  print(i)
+  bclip_ub<-lasclip(bclip_ub,treeoutub@polygons[[bigtrees_ub[i]]]@Polygons[[1]],inside=F)}
+#Error message as only one tree over 7m 
+plot(bclip_ub)
+
+plot(as.raster(grid_canopy(bclip_ub,res=0.5))-(crop(as.raster(grid_terrain(bclip_ub,method='knnidw',res=0.5)),as.raster(grid_canopy(bclip_ub,res=0.5)))))
