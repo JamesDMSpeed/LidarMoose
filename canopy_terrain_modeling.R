87f15de028b2deb31b09a8f5ba9365ed16622cfd
ingridsnoan <ingribsn@ntnu.no>
2018-10-12 09:23:52 +0200

    Only small changes

--- canopy_terrain_modeling.R
+++ canopy_terrain_modeling.R
@@ -7,24 +7,23 @@ require(rasterVis)
 bratsberg_b <- readLAS('C:/Users/Ingrid/Documents/Master - Sustherb/LidarMoose/Trondelag/clipped_las/bratsberg_b.las')
 bratsberg_ub <- readLAS('C:/Users/Ingrid/Documents/Master - Sustherb/LidarMoose/Trondelag/clipped_las/bratsberg_ub.las')
 
-#Make canopy and terrain TIN models for each treatment. TIN:representation if a continous surface.
+#Make canopy and terrain TIN models for each treatment. TIN:representation of a continous surface.
 
 
-MySites <- c("bratsberg_b", 
+#MySites <- c("bratsberg_b", 
              "bratsberg_ub")
-MySites <- as.factor(MySites)
-unique(MySites)
+#MySites <- as.factor(MySites)
+#unique(MySites)
 
-for(i in levels(MySites)){
-  terrainmod <- grid_terrain(i,method='knnidw',res=1)
-}
+#for(i in levels(MySites)){
+#  terrainmod <- grid_terrain(i,method='knnidw',res=1)
+#}
 
-terrainmod <- grid_terrain(paste(bratsberg_b),                method='knnidw',res=1)
-terrainmod <- grid_terrain(paste(bratsberg_b),                method='knnidw',res=1)
-terrainmod <- grid_terrain(paste(bratsberg_b),                method='knnidw',res=1)
-terrainmod <- grid_terrain(paste(bratsberg_gygyb),            method='knnidw',res=1)
-terrainmod <- grid_terrain(paste(bratsbegugugugukukgukurg_b), method='knnidw',res=1)
+#Manuelt for hvert område
+terrainmod_bratsberg_b  <-grid_terrain(bratsberg_b, method='knnidw',res=1)
+terrainmod_bratsberg_ub <-grid_terrain(bratsberg_ub,method='knnidw',res=1)
 
 
+canopymod_bratsberg_b   <-grid_canopy(bratsberg_b,res=1)
+canopymo_bratsberg_ub   <-grid_canopy(bratsberg_ub,res=1)
 
-canopymod<-grid_canopy(lidexclosure,res=1)
