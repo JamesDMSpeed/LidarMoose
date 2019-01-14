ea8e8d12cfa19b887c0a8a35121b269df24d91d5
ingridsnoan <ingribsn@ntnu.no>
2019-01-14 15:30:13 +0100

    trying to make table

--- Making table.R
+++ Making table.R
@@ -89,14 +89,40 @@ truls_holm_ub_canopyheight         <-  raster('Hedmark_Akershus/canopy_height_cl
 
 
 #Make a table for some summary values
-df1<-data.frame(matrix(nrow=2,ncol=3))
-rownames(df1)<-c('bratsberg_b','bratsberg_ub','hi_tydal_b','hi_tydal_ub','malvik_b','malvik_ub','namdalseid_1kub_b','namdalseid_1kub_ub','nsb_verdal_b','nsb_verdal_b','selbu_flub_b','selbu_flub_ub','selbu_kl_b','selbu_kl_ub','selbu_sl_b','selbu_sl_ub','singsaas_b','singsaas_ub','sl_tydal_b','sl_tydal_ub','steinkjer_1BBb_b','steinkjer_1BBb_ub','steinkjer_2BBb_b','steinkjer_2BBb_ub','sub_namdalseid_b','sub_namdalseid_ub','verdal1_b','verdal1_ub','verdal2_b','verdal2_ub',  )
-colnames(df1)<-c('Site name','Treatment','Median canopy height')  
-df1[1,3]<-median(getValues(bratsberg_b_canopyheight),na.rm=T)
-write.csv(df1,'mytable.csv')
+df1<-data.frame(matrix(nrow=74,ncol=6))
+rownames(df1)<-c('BRB','BRUB','HIB','HIUB' ,'MAB','MAUB', '1KB','1KUB','1NSB','1NSUB','FLB','FLUB','KLB','KLUB','SLB','SLUB', 'LAB','LAUB','SEB','SEUB','1BBB','1BBUB','2BBB','2BBUB','1SB','1SUB','1VBB','1VBUB','2VBB','2VBUB','1DRB','1DRUB','3DRB','3DRUB','4DRB','4DRUB','1FRB','1FRUB','2FRB','2FRUB','1FYB','1FYUB','1KVB','1KVUB','2KVB','2KVUB','3KVB','3KVUB','1CAB','1CAUB','2CAB','2CAUB','3NOB','3NOUB','5NOB','5NOUB','6NOB','6NOUB','DHB','DHUB','STSKNB','STSKNUB','FKB','FKUB','HPB','HPUB','SSAB','SSAUB','SSBB','SSBUB','SDB','SDUB','THB','THUB')
+colnames(df1)<-c('Treatment','Mean','Median','SD','Min','Max')  
+df1[1,2:6] <- MySummary(bratsberg_b_canopyheight)
+df1[2,2:6] <- MySummary(bratsberg_ub_canopyheight)
+df1[3,2:6] <- MySummary(hi_tydal_b_canopyheight)
 
-#calculate mean canopy height?
-cellStats(bratsberg_b_canopyheight, stat = 'mean')
-cellStats(bratsberg_ub_canopyheight, stat = 'mean')
-cellStats(bratsberg_b_canopyheight, stat = 'sd')
-cellStats(bratsberg_ub_canopyheight, stat = 'sd')
+df1
+#Error
+for (dfi(i, 2:6)){
+  MySummary(MyList[i])
+}
+#df1[1,3]<-median(getValues(bratsberg_b_canopyheight),na.rm=T)
+#write.csv(df1,'mytable.csv')
+
+summary(bratsberg_b_canopyheight)
+
+#making a summary function with the values I want
+MySummary <- function(bratsberg_b_canopyheight) c(mn = mean(getValues(bratsberg_b_canopyheight), na.rm=T), md = median(getValues(bratsberg_b_canopyheight), na.rm=T), sd= cellStats(bratsberg_b_canopyheight, stat='sd', na.rm=T), min= min(getValues(bratsberg_b_canopyheight), na.rm=T),max= max(getValues(bratsberg_b_canopyheight), na.rm=T) )
+MySummary(bratsberg_b_canopyheight)
+
+
+MySummary <- function(i) c(mn = mean(getValues(i), na.rm=T), md = median(getValues(i), na.rm=T), sd= cellStats(i, stat='sd', na.rm=T), min= min(getValues(i), na.rm=T),max= max(getValues(i), na.rm=T) )
+x <- MySummary(bratsberg_b_canopyheight)
+
+MyList <- c(bratsberg_b_canopyheight, bratsberg_ub_canopyheight, hi_tydal_b_canopyheight, hi_tydal_ub_canopyheight)
+
+
+#preallocate the space for the values you want to store.
+testmat <-  matrix(nrow=37, ncol = 5)
+
+for(i in MyList) {
+  testmat[1,] <- MySummary(i)
+  print(i)
+}
+
+#still only saves result of last iteration
