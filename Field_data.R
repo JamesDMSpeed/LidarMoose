4e822c4a1dd3f8fe7a63e1813f8436da9c84afbd
ingridsnoan <ingribsn@ntnu.no>
2019-02-13 13:59:12 +0100

    Have filtered the dataframe so that only necessary info is left, trying to calculate median from field data

--- Field_data.R
+++ Field_data.R
@@ -20,37 +20,85 @@ study_sites<-c('BRB','BRUB','HIB','HIUB' ,'MAB','MAUB', '1KB','1KUB','1NSB','1NS
 
 dat2 <- dat1[(dat1$LocalityCode==study_sites),]
 
-
-# untable for å lage en rad for hvert individ.
-library(reshape)
-dat3 <- reshape::untable(dat2, num = dat2$Quantity)
-
-
-#Filter: remove years that are not from the same year as LIDAR data for each site
+# Remove years that are not from the same year as LIDAR data for each site
 
 #dat3$year <- NA
 #bratsberg_fd <-dat3[dat3$LocalityName=='Bratsberg'& grepl('2017',dat3$`_Date`),] 
 #singsaas_fd <-dat3[dat3$LocalityName=='Singsås'& grepl('2015',dat3$`_Date`),] 
 
 #Make col with years
-dat3$`_Date` <- as.Date(dat3$`_Date`, "%d.%m.%Y")
-dat3$year <- format(as.Date(dat3$`_Date`, format="%d/%m/%Y"),"%Y")
+dat2$`_Date` <- as.Date(dat2$`_Date`, "%d.%m.%Y")
+dat2$year <- format(as.Date(dat2$`_Date`, format="%d/%m/%Y"),"%Y")
 
 #Select data from year of interest
+#Trøndelag
+dat3 <- dat2[!(dat2$LocalityName=='Bratsberg'& dat2$year!='2017'),]
+dat3 <- dat3[!(dat3$LocalityName=='Singsås' & dat3$year!='2015'),]
+dat3 <- dat3[!(dat3$LocalityName=='Malvik' & dat3$year!='2016'),]
+dat3 <- dat3[!(dat3$LocalityName=='Selbu_Flub' & dat3$year!='2015'),]
+dat3 <- dat3[!(dat3$LocalityName=='Selbu_kl' & dat3$year!='2015'),]
+dat3 <- dat3[!(dat3$LocalityName=='Selbu_Sl' & dat3$year!='2015'),]
+dat3 <- dat3[!(dat3$LocalityName=='Hi_tydal' & dat3$year!='2015'),]
+dat3 <- dat3[!(dat3$LocalityName=='Sl_Tydal' & dat3$year!='2015'),]
+dat3 <- dat3[!(dat3$LocalityName=='steinkjer_1BBb' & dat3$year!='2011'),]
+dat3 <- dat3[!(dat3$LocalityName=='steinkjer_2BBb' & dat3$year!='2011'),]
+dat3 <- dat3[!(dat3$LocalityName=='Nsb_Verdal' & dat3$year!='2015'),]
+dat3 <- dat3[!(dat3$LocalityName=='verdal_1vb' & dat3$year!='2015'),]
+dat3 <- dat3[!(dat3$LocalityName=='verdal_2VB' & dat3$year!='2015'),]
+dat3 <- dat3[!(dat3$LocalityName=='namdalseid_1kub' & dat3$year!='2010'),]
+dat3 <- dat3[!(dat3$LocalityName=='Sub_Namdalseid' & dat3$year!='2010'),]
 
+#Telemark
+dat3 <- dat3[!(dat3$LocalityName=='Fritsøe1' & dat3$year!='2017'),]
+dat3 <- dat3[!(dat3$LocalityName=='Fritsøe2' & dat3$year!='2017'),]
+dat3 <- dat3[!(dat3$LocalityName=='Nome_Cappelen1' & dat3$year!='2017'),]
+dat3 <- dat3[!(dat3$LocalityName=='Nome_Cappelen2' & dat3$year!='2017'),]
+dat3 <- dat3[!(dat3$LocalityName=='Notodden3' & dat3$year!='2017'),]
+dat3 <- dat3[!(dat3$LocalityName=='Notodden5' & dat3$year!='2017'),]
+dat3 <- dat3[!(dat3$LocalityName=='Notodden6' & dat3$year!='2017'),]
+dat3 <- dat3[!(dat3$LocalityName=='Drangedal1' & dat3$year!='2017'),]
+dat3 <- dat3[!(dat3$LocalityName=='Drangedal3' & dat3$year!='2016'),]
+dat3 <- dat3[!(dat3$LocalityName=='Drangedal4' & dat3$year!='2016'),]
+dat3 <- dat3[!(dat3$LocalityName=='Kviteseid1' & dat3$year!='2017'),]
+dat3 <- dat3[!(dat3$LocalityName=='Kviteseid2' & dat3$year!='2017'),]
+dat3 <- dat3[!(dat3$LocalityName=='Kviteseid3' & dat3$year!='2017'),]
+dat3 <- dat3[!(dat3$LocalityName=='Furesdal' & dat3$year!='2017'),]
+
+#Hedmark_Akershus
+dat3 <- dat3[!(dat3$LocalityName=='Didrik Holmsen' & dat3$year!='2018'),]
+dat3 <- dat3[!(dat3$LocalityName=='Stangeskovene Aurskog' & dat3$year!='2018'),]
+dat3 <- dat3[!(dat3$LocalityName=='Stig Dæhlen' & dat3$year!='2017'),]
+dat3 <- dat3[!(dat3$LocalityName=='Truls Holm' & dat3$year!='2017'),]
+dat3 <- dat3[!(dat3$LocalityName=='Fet 3' & dat3$year!='2016'),]
+dat3 <- dat3[!(dat3$LocalityName=='Eidskog' & dat3$year!='2016'),]
+dat3 <- dat3[!(dat3$LocalityName=='Halvard Pramhus' & dat3$year!='2016'),]
+dat3 <- dat3[!(dat3$LocalityName=='Stangeskovene Eidskog' & dat3$year!='2016'),]
+
+
+
+
+
+# untable for å lage en rad for hvert individ.
+library(reshape)
+dat4 <- reshape::untable(dat3, num = dat3$Quantity)
 
 
 
 # konvertere height_class til m
-max(dat3$Height_class_50cm)
+max(dat4$Height_class_50cm)
 #max = 7
-dat3$height_m <- ifelse(dat3$Height_class_50cm=='1', 0.25,
-                        ifelse(dat3$Height_class_50cm=='2', 0.75,
-                               ifelse(dat3$Height_class_50cm=='3', 1.25,
-                                      ifelse(dat3$Height_class_50cm=='4', 1.75,
-                                             ifelse(dat3$Height_class_50cm=='5', 2.25,
-                                                    ifelse(dat3$Height_class_50cm=='6', 2.75, 3.25))))))
+dat4$height_m <- ifelse(dat4$Height_class_50cm=='1', 0.25,
+                        ifelse(dat4$Height_class_50cm=='2', 0.75,
+                               ifelse(dat4$Height_class_50cm=='3', 1.25,
+                                      ifelse(dat4$Height_class_50cm=='4', 1.75,
+                                             ifelse(dat4$Height_class_50cm=='5', 2.25,
+                                                    ifelse(dat4$Height_class_50cm=='6', 2.75, 3.25))))))
 
 
 # aggregate to plot, calculate median height plot
+test <- reshape2::dcast(dat4, LocalityName ~ height_m)
+test$median <- 
+
+
+
 
